a60fd36bdf960496c85f0e6fc241ebf265203594
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2026-01-29 20:22:51 +0700

    Initial framing

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
-console.log("Hello world!");
+Gameboard = (function () {
+  const board = ["", "", "", "", "", "", "", "", ""];
+
+  const getBoard = () => board;
+
+  return { getBoard };
+})();
+
+function Player(mark) {
+  const score = 0;
+
+  const incrementScore = () => score++;
+  const getScore = () => score;
+
+  return {
+    mark,
+    incrementScore,
+    getScore,
+  };
+}
+
+RoundController = (function () {
+  const player1 = Player("X");
+  const player2 = Player("O");
+
+  console.log(player1);
+  console.log(player2);
+})();
